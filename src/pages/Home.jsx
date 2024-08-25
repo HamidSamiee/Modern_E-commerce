4081c403974ed81eca3693ab1672fdc24f01348a
HamidSamiee <hamidsmoasers@yahoo.com>
2024-08-25 15:09:21 +0330

    create layout Component +header+Footer +pages(Home+About+Contact) and routes for react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  return (
+    <div className="bg-amber-200 w-full h-full">Home</div>
+  )
+}
+
+export default Home
---
+++ src/routes/routes.jsx
@@ -0,0 +1,11 @@
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+
+
+const routes=[
+    {path:"about" ,element:<About />},
+    {path:"contact" ,element:<Contact />},
+]
+
+
+export default routes;
