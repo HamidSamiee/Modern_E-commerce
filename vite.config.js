350e1167de41b353f190066435fb9304e9268a96
HamidSamiee <hamidsmoasers@yahoo.com>
2024-08-25 14:44:25 +0330

    install dependencies and add Absolute paths (vite.config.js & jsconfig.json)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      "@components": "/src/components",
+      "@layout": "/src/layout",
+      "@features": "/src/features",
+      "@pages": "/src/pages",
+      "@util": "/src/util",
+      "@hooks": "/src/hooks",
+      "@assets": "/src/assets",
+      "@ui": "/src/ui",
+      "@services": "/src/services",
+      "@routes": "/src/routes",
+    },
+  },
+});
