ac4781a0447bb5ed26e0835f6287159979772a33
HamidSamiee <hamidsmoasers@yahoo.com>
2024-09-23 19:40:18 +0330

    alert path store.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './app/store.jsx/index.js'
+import { store } from './app/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
